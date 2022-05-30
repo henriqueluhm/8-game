d2cb109f1d12d50bb006abfd4a1cec640e7a8fe3
henrique luhm <henriqueluhmwork@gmail.com>
2022-05-30 00:58:35 -0300

    feat: created shaking animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {
+      animation: {
+        shake: 'shake 350ms ease-in-out',
+      },
+      keyframes: {
+        shake: {
+          "0%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(4px)" },
+          "50%": { transform: "translateX(-3px)" },
+          "75%": { transform: "translateX(2px)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+    },
     boxShadow: {
       default: "0 0 0.20em 0 #0abdc6, inset 0 0 0.20em 0 #0abdc6",
       hover: "0 0 0.40em 0 #0abdc6, inset 0 0 0.40em 0 #0abdc6",
