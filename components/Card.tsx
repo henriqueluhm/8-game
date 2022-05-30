99d426967f170a20be2e74c1c0f99f1b37c0783c
henrique luhm <henriqueluhmwork@gmail.com>
2022-05-30 00:59:22 -0300

    feat: added shaking animation in cards when it cannot move

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,19 @@
 import classNames from "classnames";
-import React, { ReactElement, useState } from "react";
+import React, {
+  LegacyRef,
+  ReactElement,
+  RefObject,
+  useRef,
+  useState,
+} from "react";
 import Draggable from "react-draggable";
+import { ICard } from "./Board";
 
-const Card = ({ cardContent }: { cardContent: string }): ReactElement => {
+const Card = ({ card }: { card: ICard }): ReactElement => {
   const [isDragging, setIsDragging] = useState(false);
+  const [isCLicking, setIsClicking] = useState(false);
+
+  const cardRef: LegacyRef<HTMLDivElement> | null = useRef(null);
 
   const onStartDragging = (): void => {
     setIsDragging(true);
@@ -14,17 +24,26 @@ const Card = ({ cardContent }: { cardContent: string }): ReactElement => {
   };
 
   return (
-    <Draggable onStart={onStartDragging} onStop={onStopDragging}>
+    <Draggable
+      onStart={onStartDragging}
+      onStop={onStopDragging}
+      bounds="parent"
+      disabled
+      nodeRef={cardRef as unknown as RefObject<HTMLElement>}
+    >
       <div
+        ref={cardRef}
+        onMouseDownCapture={() => setIsClicking(true)}
+        onAnimationEnd={() => setIsClicking(false)}
         className={classNames(
           "flex items-center justify-center col-span-1 row-span-1 transition-shadow duration-150 border-2 rounded-md border-game-light-blue cursor-grab shadow-default hover:shadow-hover",
-          { "cursor-grabbing": isDragging }
+          { "cursor-grabbing": isDragging, "animate-shake": isCLicking }
         )}
         style={{
           textShadow: "0 0 0.20em #0abdc6",
         }}
       >
-        {cardContent !== "-" && cardContent}
+        {card.content !== "-" && card.content}
       </div>
     </Draggable>
   );
